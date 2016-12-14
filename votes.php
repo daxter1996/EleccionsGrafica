8556e16b81c48c9dfb66792feaa8976c4c87dcf8
daxter1996 <jesuslizalles@gmail.com>
2016-12-14 20:08:45 +0100

    Suma de tots els vots per partits

--- votes.php
+++ votes.php
@@ -11,33 +11,25 @@
 
 <?php
 
-   // echo print_r($_POST);
     $arrayGeneral = [];
     $tableInfo = ["Blanks","Nulls"];
+
 
     for($i = 0;$i<$_POST["parties"];$i++){
         array_push($tableInfo,$_POST["party-".$i]);
     }
 
-    /*for($i = 1;$i<=$_POST["towns"];$i++){
-        echo "<hr/>". $_POST[$i."-0/"] . "</br>";
-        for($j = 1;$j<=$_POST["parties"]+2;$j++){
-            echo $tableInfo[$j-1]." ";
-            echo " ".$_POST[$i."-".$j."/"] . "<br/>";
-        }
-    }*/
+    for($i = 0;$i<$_POST["parties"]+2;$i++){
+        $arrayGeneral[$tableInfo[$i]]= 0;
+    }
 
     for($i = 1;$i<=$_POST["towns"];$i++){
-        //echo "<hr/>". $_POST[$i."-0/"] . "</br>";
-        $tmp = [];
         for($j = 1;$j<=$_POST["parties"]+2;$j++){
-            $tmp[$tableInfo[$j-1]] = $_POST[$i."-".$j."/"];
+            $arrayGeneral[$tableInfo[$j-1]] += $_POST[($i)."-".($j)."/"];
         }
-        $arrayGeneral[$_POST[$i."-0/"]] = $tmp;
     }
 
     echo print_r($arrayGeneral);
-
 ?>
 
 </body>
